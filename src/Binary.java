84c3981cbfd430d3730486224f362fd1ef31c8c9
SuzanStockey <suzan-sp@hotmail.com>
2022-10-13 18:48:47 -0300

    Initial commit

---
+++ src/Binary.java
@@ -0,0 +1,79 @@
+import java.nio.charset.StandardCharsets;
+
+public class Binary {
+    private Integer bits = 0;
+    private String value = "";
+
+    public boolean isBinary(){
+        for(int i = 0; i < value.length(); i++){
+            if(!"1".equalsIgnoreCase(String.valueOf(value.charAt(i))) && !"0".equalsIgnoreCase(String.valueOf(value.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String valueDecimalNoSignal(){
+        while(value.length() < bits){
+            value = "0" + value;
+        }
+        Double decimalNoSignal = 0.0;
+        for (int i = 0; i < value.length(); i++){
+            if("1".equalsIgnoreCase(String.valueOf(value.charAt(i)))) {
+                decimalNoSignal += Math.pow(2, (value.length() - 1 - i));
+            }
+        }
+        return String.format("%.0f", decimalNoSignal);
+    }
+
+    public String valueDecimal2Complement(){
+        while(value.length() < bits){
+            value = "0" + value;
+        }
+
+        Double decimal2Complement = 0.0;
+        String sinal = "";
+        for (int i = 0; i < value.length(); i++){
+            if (i == 0){
+                if ("1".equalsIgnoreCase(String.valueOf(value.charAt(i)))){
+                    sinal = "-";
+                }
+            } else {
+                if ("1".equalsIgnoreCase(String.valueOf(value.charAt(i)))) {
+                    decimal2Complement += Math.pow(2, (value.length() - 1 - i));
+                }
+            }
+        }
+        return sinal + String.format("%.0f", decimal2Complement);
+    }
+
+    public String character() {
+        if (bits > value.length()){
+            while(value.length() < bits){
+                value = "0" + value;
+            }
+        }
+        byte[] ret = new byte[value.length() / 8];
+        for (int i = 1; i <= ret.length; i++) {
+            String chunk = value.substring(i * 8, i * 8 + 8);
+            ret[i] = (byte) Short.parseShort(chunk, 2);
+        }
+        return new String(ret, StandardCharsets.UTF_8);
+    }
+
+    public Integer getBits() {
+        return bits;
+    }
+
+    public void setBits(Integer bits) {
+        this.bits = bits;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+/**
+ * Programa que obtém um valor em binário e sua quantidade de bits, e retorna seu valor em decimal sem sinal, decimal em com7plemento de 2 e, caso tenha 8 bits ou menos, o caractere que representa no UTF-8
+ *
+ * @author Suzan Stockey Pereira
+ * @version 13102022
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner in = new Scanner(System.in);
+
+        String continuar = "S";
+
+        while (!"N".equalsIgnoreCase(continuar)) {
+            while (!"S".equalsIgnoreCase(continuar)) {
+                System.out.println("Deseja continuar?(S/N)");
+                continuar = in.next();
+            }
+            System.out.println("Informe o valor em binário: ");
+            Binary binario = new Binary();
+            binario.setValue(in.next());
+            System.out.println("Informe o número de bits: ");
+            binario.setBits(in.nextInt());
+
+            if (binario.getValue().length() > binario.getBits()) {
+                System.out.println("O binário excede o número de bits informado.");
+            } else if (binario.isBinary()) {
+                System.out.printf("Valor em decimal sem sinal: %s\n", binario.valueDecimalNoSignal());
+                System.out.printf("Valor em complemento de 2: %s\n", binario.valueDecimal2Complement());
+                if (binario.getBits() < 9) {
+                    System.out.printf("Caractere representado: %s\n", binario.character());
+                }
+            } else {
+                System.out.println("O valor informado não é um binário.");
+            }
+            System.out.println("Deseja continuar?(S/N)");
+            continuar = in.next();
+        }
+    }
+}
