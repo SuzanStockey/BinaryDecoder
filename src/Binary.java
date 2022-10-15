1b02966448513274a4fcc887bf130a91515897aa
SuzanStockey <suzan-sp@hotmail.com>
2022-10-14 21:40:29 -0300

    Ajustes no método character() e correção do método valueDecimal2Complement() para calcular o valor corretamente (estava calculando o sinal magnitude ao invés de complemento de 2)

--- src/Binary.java
+++ src/Binary.java
@@ -3,6 +3,7 @@ import java.nio.charset.StandardCharsets;
 public class Binary {
     private Integer bits = 0;
     private String value = "";
+    private double decimalNoSignal = 0.0;
 
     public boolean isBinary(){
         for(int i = 0; i < value.length(); i++){
@@ -17,7 +18,6 @@ public class Binary {
         while(value.length() < bits){
             value = "0".concat(value);
         }
-        double decimalNoSignal = 0.0;
         for (int i = 0; i < value.length(); i++){
             if("1".equalsIgnoreCase(String.valueOf(value.charAt(i)))) {
                 decimalNoSignal += Math.pow(2, (value.length() - 1 - i));
@@ -34,31 +34,19 @@ public class Binary {
         double decimal2Complement = 0.0;
         String sinal = "";
         for (int i = 0; i < value.length(); i++){
-            if (i == 0){
-                if ("1".equalsIgnoreCase(String.valueOf(value.charAt(i)))){
-                    sinal = "-";
-                }
-            } else {
-                if ("1".equalsIgnoreCase(String.valueOf(value.charAt(i)))) {
+            if ("1".equalsIgnoreCase(String.valueOf(value.charAt(i)))) {
+                if (i == 0) {
+                    decimal2Complement -= Math.pow(2, (value.length() - 1 - i));
+                } else {
                     decimal2Complement += Math.pow(2, (value.length() - 1 - i));
                 }
             }
         }
-        return sinal + String.format("%.0f", decimal2Complement);
+        return String.format("%.0f", decimal2Complement);
     }
 
     public String character() {
-        if (bits > value.length()){
-            while(value.length() < bits){
-                value = "0".concat(value);
-            }
-        }
-        byte[] ret = new byte[value.length() / 8];
-        for (int i = 0; i < ret.length; i++) {
-            String chunk = value.substring(i * 8, i * 8 + 8);
-            ret[i] = (byte) Short.parseShort(chunk, 2);
-        }
-        return new String(ret, StandardCharsets.UTF_8);
+        return String.valueOf(Character.toChars(Integer.parseInt(String.format("%.0f", decimalNoSignal))));
     }
 
     public Integer getBits() {
